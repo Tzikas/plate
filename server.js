ef3fb08aaa1e87b90f1b99d07de81d039aaa7f45
squirrl <niko.tzikas@gmail.com>
2017-05-28 14:15:33 -0400

    hmm

--- server.js
+++ server.js
@@ -84,9 +84,9 @@ if (isDev) {
   }));
 
   app.use(webpackHotMiddleware(compiler));
-  app.use(express.static(path.resolve(__dirname, '../dist/public')));
+  app.use(express.static(path.resolve(__dirname, '../dist')));
 } else {
-  app.use(express.static(path.resolve(__dirname, '../dist/public')));
+  app.use(express.static(path.resolve(__dirname, '../dist')));
   app.get('*', function (req, res) {
     res.sendFile(path.resolve(__dirname, '../dist/public/index.html'));
     res.end();
